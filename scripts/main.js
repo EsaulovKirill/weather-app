a5d26633bddccd897b36d12186e75b5790ce545e
EsaulovKirill <esaulovofficial@yandex.ru>
2026-07-04 22:03:06 +0300

    Add mock weather rendering

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,8 @@ unitToggle.addEventListener("click", function (event) {
   clickedButton.classList.add("weather-controls__unit-button--active");
 });
 
+//========================================================
+
 const periodToggle = document.querySelector(".weather-controls__period-toggle");
 
 periodToggle.addEventListener("click", function (event) {
@@ -48,3 +50,55 @@ periodToggle.addEventListener("click", function (event) {
   activeButton.classList.remove("weather-controls__period-button--active");
   clickedButton.classList.add("weather-controls__period-button--active");
 });
+
+//========================================================
+
+const form = document.querySelector(".weather-controls__search-form");
+const searchInput = document.querySelector(".weather-controls__search");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const city = searchInput.value.trim();
+
+  if (!city) {
+    return;
+  }
+  console.log(city);
+  const searchedWeather = {
+    ...mockWeather,
+    city: city
+  }
+
+  renderWeatherCard(searchedWeather)
+  searchInput.value = "";
+  searchInput.focus();
+});
+
+
+const mockWeather = {
+  city: "Mosc222ow",
+  date: "Tuesday, 20:52",
+  condition: "Cloudy",
+  temperature: "293°C",
+  wind: "0 km/h",
+  humidity: "0%",
+  feelsLike: "0°C",
+};
+
+const cityElement = document.querySelector('.weather-card__city')
+const dateElement = document.querySelector('.weather-card__date')
+const conditionElement = document.querySelector('.weather-card__condition')
+const temperatureElement = document.querySelector('.weather-card__temperature')
+const windElement = document.querySelector('.weather-card__wind')
+const humidityElement = document.querySelector('.weather-card__humidity')
+const feelsLikeElement = document.querySelector('.weather-card__feels-like')
+
+function renderWeatherCard (weatherData) {
+  cityElement.textContent = weatherData.city
+  dateElement.textContent = weatherData.date
+  conditionElement.textContent = weatherData.condition
+  temperatureElement.textContent = weatherData.temperature
+  windElement.textContent = weatherData.wind
+  humidityElement.textContent = weatherData.humidity
+  feelsLikeElement.textContent = weatherData.feelsLike
+}
+renderWeatherCard(mockWeather)
