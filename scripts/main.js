ddf6d55ab141ede22d087473ce04272da9d782c6
EsaulovKirill <esaulovofficial@yandex.ru>
2026-07-04 23:21:12 +0300

    Connect weather API search

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,40 @@
-const API_KEY = "";
-
+//========================================================
+// API
+const API_KEY = "38d2c232dcaa66f3d3ec7034b86ecc70";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${API_KEY}&units=metric&lang=en`;
 
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+//========================================================
+// DOM Elements
+
+const unitToggle = document.querySelector(".weather-controls__unit-toggle");
+const periodToggle = document.querySelector(".weather-controls__period-toggle");
+
+const form = document.querySelector(".weather-controls__search-form");
+const searchInput = document.querySelector(".weather-controls__search");
+
+const cityElement = document.querySelector(".weather-card__city");
+const dateElement = document.querySelector(".weather-card__date");
+const conditionElement = document.querySelector(".weather-card__condition");
+const temperatureElement = document.querySelector(".weather-card__temperature");
+const windElement = document.querySelector(".weather-card__wind");
+const humidityElement = document.querySelector(".weather-card__humidity");
+const feelsLikeElement = document.querySelector(".weather-card__feels-like");
 
 //========================================================
-const unitToggle = document.querySelector(".weather-controls__unit-toggle");
+// Mock data
+
+const mockWeather = {
+  city: "Введи город",
+  date: "сегодня",
+  condition: "Кайфово",
+  temperature: "23°C",
+  wind: "5 km/h",
+  humidity: "80%",
+  feelsLike: "25°C",
+};
+
+//========================================================
+// Event Listeners
 
 unitToggle.addEventListener("click", function (event) {
   const clickedButton = event.target.closest(".weather-controls__unit-button");
@@ -20,24 +47,26 @@ unitToggle.addEventListener("click", function (event) {
   ) {
     return;
   }
+
   const activeButton = unitToggle.querySelector(
     ".weather-controls__unit-button--active",
   );
+
   activeButton.classList.remove("weather-controls__unit-button--active");
   clickedButton.classList.add("weather-controls__unit-button--active");
 });
 
 //========================================================
 
-const periodToggle = document.querySelector(".weather-controls__period-toggle");
-
 periodToggle.addEventListener("click", function (event) {
   const clickedButton = event.target.closest(
     ".weather-controls__period-button",
   );
+
   if (!clickedButton) {
     return;
   }
+
   if (
     clickedButton.classList.contains("weather-controls__period-button--active")
   ) {
@@ -47,58 +76,70 @@ periodToggle.addEventListener("click", function (event) {
   const activeButton = periodToggle.querySelector(
     ".weather-controls__period-button--active",
   );
+
   activeButton.classList.remove("weather-controls__period-button--active");
   clickedButton.classList.add("weather-controls__period-button--active");
 });
 
 //========================================================
 
-const form = document.querySelector(".weather-controls__search-form");
-const searchInput = document.querySelector(".weather-controls__search");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const city = searchInput.value.trim();
 
-  if (!city) {
-    return;
-  }
-  console.log(city);
-  const searchedWeather = {
-    ...mockWeather,
-    city: city
-  }
-
-  renderWeatherCard(searchedWeather)
-  searchInput.value = "";
-  searchInput.focus();
+  handleSearch();
 });
 
+//========================================================
+// Functions
 
-const mockWeather = {
-  city: "Mosc222ow",
-  date: "Tuesday, 20:52",
-  condition: "Cloudy",
-  temperature: "293°C",
-  wind: "0 km/h",
-  humidity: "0%",
-  feelsLike: "0°C",
-};
+function handleSearch() {
+  const city = searchInput.value.trim();
 
-const cityElement = document.querySelector('.weather-card__city')
-const dateElement = document.querySelector('.weather-card__date')
-const conditionElement = document.querySelector('.weather-card__condition')
-const temperatureElement = document.querySelector('.weather-card__temperature')
-const windElement = document.querySelector('.weather-card__wind')
-const humidityElement = document.querySelector('.weather-card__humidity')
-const feelsLikeElement = document.querySelector('.weather-card__feels-like')
+  if (!city) return;
+  
+  fetchWeather(city);
 
-function renderWeatherCard (weatherData) {
-  cityElement.textContent = weatherData.city
-  dateElement.textContent = weatherData.date
-  conditionElement.textContent = weatherData.condition
-  temperatureElement.textContent = weatherData.temperature
-  windElement.textContent = weatherData.wind
-  humidityElement.textContent = weatherData.humidity
-  feelsLikeElement.textContent = weatherData.feelsLike
+  searchInput.value = "";
+  searchInput.focus();
 }
-renderWeatherCard(mockWeather)
+
+function renderWeatherCard(weatherData) {
+  cityElement.textContent = weatherData.city;
+  dateElement.textContent = weatherData.date;
+  conditionElement.textContent = weatherData.condition;
+  temperatureElement.textContent = weatherData.temperature;
+  windElement.textContent = weatherData.wind;
+  humidityElement.textContent = weatherData.humidity;
+  feelsLikeElement.textContent = weatherData.feelsLike;
+}
+
+async function fetchWeather(city) {
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric&lang=ru`;
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!response.ok) {
+  console.log("City not found");
+  return;
+}
+
+  const transformedData = transformWeatherData(data);
+  console.log(transformedData);
+  renderWeatherCard(transformedData);
+}
+
+function transformWeatherData(data){
+  return {
+    city: data.name,
+    date: "Today",
+    condition: data.weather[0].main,
+    temperature: `${Math.round(data.main.temp)}°C`,
+    wind: `${data.wind.speed} m/s`,
+    humidity: `${data.main.humidity}%`,
+    feelsLike: `${Math.round(data.main.feels_like)}°C`,
+  }
+}
+
+//========================================================
+// Init
+
+renderWeatherCard(mockWeather);
